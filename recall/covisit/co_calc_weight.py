55df4f3e020691d72e59807a144bc4e6cabf1fc5
顾剑波 <gujianbobo@gmail.com>
2023-04-12 17:19:12 +0800

    co

--- recall/covisit/co_calc_weight.py
+++ recall/covisit/co_calc_weight.py
@@ -15,7 +15,13 @@ logging.basicConfig(filename=config.log_file, level=logging.DEBUG, format=LOG_FO
 
 def calc_weight(input_file, output_file):
     pair_df = pd.read_csv(input_file, sep=",")
-
+    pair_df = pair_df.sort_values(['item1', 'weight'], ascending=[True, False])
+    pair_df = pair_df.reset_index()
+    pair_df['n'] = pair_df.groupby('item1').item2.cumcount()
+    pair_df = pair_df.loc[pair_df.n < 100].drop('n', axis=1)
+    with open(output_file, "w") as fd:
+        for row in pair_df.iterrows():
+            fd.write(f"{row['item1']}\t{row['item2']}\t{row['score']}\n")
 
 
 if __name__ == "__main__":
