432e36578ffe25ea82d3ef378f8531e5b204f5c4
顾剑波 <gujianbobo@gmail.com>
2023-04-29 20:24:27 +0800

    pre

--- recall/candidation/get_candi.sh
+++ recall/candidation/get_candi.sh
@@ -10,7 +10,7 @@ echo ${out_folder}
 
 # 生成item pair
 nohup python -u get_candidation.py --input_file ${root}/data/sessions_train.csv \
-  --output_file ${root}/cand/candi_100_top100.csv --single_topk 50 \
+  --output_file ${root}/cand/candi_100_top100.csv --single_topk 100 \
   --product_file ${root}/data/products_train.csv \
   --root_path ${root} --topk 100 \
   --log_file ${root}/log/candi.log >> candi.log 2>&1 &
