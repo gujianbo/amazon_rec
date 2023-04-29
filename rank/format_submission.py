13aac20b50c955ced56641d607b4718da538fee7
顾剑波 <gujianbobo@gmail.com>
2023-04-29 16:20:00 +0800

    sub

--- rank/format_submission.py
+++ rank/format_submission.py
@@ -9,19 +9,21 @@ import pandas as pd
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(filename)s[line:%(lineno)d]- %(message)s"
 logging.basicConfig(filename=config.log_file, level=logging.DEBUG, format=LOG_FORMAT)
 
+
 def format(input_file, output_file):
     buf = []
     last_prev_items = ""
 
     out_dict = {"next_item_prediction": []}
     with open(input_file, "r") as fd:
-        for line in tqdm(fd, desc="eval"):
+        for line in tqdm(fd, desc="format"):
             (prev_items, candi, locale_code, label, pred) = line.strip().split("\t")
             if last_prev_items != "" and prev_items != last_prev_items:
                 sorted_buf = sorted(buf, key=lambda x: x[1], reverse=True)
                 # print(sorted_buf)
                 items = [item[0] for item in sorted_buf]
                 out_dict["next_item_prediction"].append(items)
+                buf = []
             buf.append([candi, float(pred)])
 
             last_prev_items = prev_items
