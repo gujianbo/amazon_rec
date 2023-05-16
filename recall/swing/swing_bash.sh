2b8bc5148b01af9ec09ff549d488d44514f1ca39
顾剑波 <gujianbobo@gmail.com>
2023-05-16 12:53:01 +0800

    add

--- recall/swing/swing_bash.sh
+++ recall/swing/swing_bash.sh
@@ -11,9 +11,9 @@ echo ${out_folder}
 
 
 # 生成item pair
-nohup python -u swing_gen_item_pair.py --input_file ${root}/data/sessions_train.csv \
+python -u swing_gen_item_pair.py --input_file ${root}/data/sessions_train.csv \
   --output_file ${root}/${out_folder}/swing.sim \
-  --log_file ${root}/log/swing_pair.log --country ${country} > log 2>&1 &
+  --log_file ${root}/log/swing_pair.log --country ${country} > log 2>&1
 
 # 对item pair进行排序后uniq，删掉只有一个的pair
 for i in $(seq 0 9)
@@ -26,11 +26,11 @@ do
 done
 
 # 根据uniq后的pair进行相似度计算
-nohup python -u swing_sim_calc.py --output_file ${root}/${out_folder}/swing.sim \
-  --log_file ${root}/log/swing_sim.log > log 2>&1 &
+python -u swing_sim_calc.py --output_file ${root}/${out_folder}/swing.sim \
+  --log_file ${root}/log/swing_sim.log > log 2>&1
 
 # 对计算的相似度进行过滤，保留每个topk
-nohup python -u swing_sim_uniq.py --output_file ${root}/${out_folder}/swing.sim \
-  --log_file ${root}/log/swing_sim.log > log 2>&1 &
+python -u swing_sim_uniq.py --output_file ${root}/${out_folder}/swing.sim \
+  --log_file ${root}/log/swing_sim.log > log 2>&1
 
 
