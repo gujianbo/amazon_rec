2287395d7658076d8bc2cc0cdb151a9dc449fd21
顾剑波 <gujianbobo@gmail.com>
2023-05-23 17:45:54 +0800

    item

--- recall/candidation/get_candi.sh
+++ recall/candidation/get_candi.sh
@@ -58,3 +58,9 @@ nohup python -u flatten_candidation.py --input_file ${root}/cand/submission_100_
   --output_file ${root}/cand/submission_100_top100.pre.flatten.csv \
   --log_file ${root}/log/flatten.log --neg_sample_rate 0 --drop_no_hit 0 > flatten.log 2>&1 &
 
+
+nohup python -u get_candidation.py --input_file ${root}/data/sessions_train.csv \
+  --output_file ${root}/cand/candi_100_top100.v4.csv --single_topk 100 \
+  --product_file ${root}/data/products_train.csv \
+  --root_path ${root} --topk 100 \
+  --log_file ${root}/log/candi.log >> candi.log 2>&1 &
