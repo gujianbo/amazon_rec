c28ee23652786e8f9d79459bce2b5741257a69ec
顾剑波 <gujianbobo@gmail.com>
2023-05-25 15:09:46 +0800

    local_i2i_feat

--- rank/xgb.sh
+++ rank/xgb.sh
@@ -11,6 +11,10 @@ nohup python -u xgb_rank.py --train_file ${root}/train_data/train --test_file ${
   --model_file ${root}/model/xgb.pre.1 --log_file ${root}/log/xgb.log --scale_pos_weight 10 \
   --num_boost_round 500 > log 2>&1 &
 
+nohup python -u xgb_rank.py --train_file ${root}/train_data/train --test_file ${root}/train_data/test \
+  --model_file ${root}/model/xgb.v5.1 --log_file ${root}/log/xgb.log --scale_pos_weight 10 \
+  --num_boost_round 500 > log 2>&1 &
+
 nohup python -u xgb_predict.py --input_file ${root}/cand/candi_feat_100.pre.test \
   --output_file ${root}/cand/candi_feat_100.pre.test.pred \
   --model_file ${root}/model/xgb.pre.1 > log 2>&1 &
@@ -25,5 +29,5 @@ python format_submission.py --input_file ${root}/cand/submission_feat_100.pre.pr
   --output_file ${root}/cand/submission_100.pre.parquet
 
 nohup python -u slice_data.py --input_file /home/zjlab/data/amazon/candi_100_top200.v5.feat \
-  --train_file /home/zjlab/data/amazon/train_data/train --test_file /home/zjlab/data/amazon/train_data/test \
+  --train_file ${root}/train_data/train --test_file ${root}/train_data/test \
   --sample_cnt 20 > log 2>&1 &
