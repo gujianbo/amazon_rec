64e1887cbd57079415b6be12f539476569a72e0b
顾剑波 <gujianbobo@gmail.com>
2023-05-25 17:09:44 +0800

    local_i2i_feat

--- rank/xgb_rank.py
+++ rank/xgb_rank.py
@@ -39,6 +39,7 @@ class Iterator(xgboost.DataIter):
                 feat += [float(item) for item in interact_feat_str.split(",")]
                 feat += [float(item) for item in local_sec_feat_str.split(",")]
                 feat += [float(item) for item in locale_code_feat_str.split(",")]
+                print(f"len:{len(feat)}")
                 train_x_arr.append(feat)
                 train_y_arr.append([float(label)])
         X = np.array(train_x_arr)
