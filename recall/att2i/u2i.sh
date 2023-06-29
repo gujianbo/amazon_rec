4f1a3ffe743d09165c3e66e8297f097f49e45685
顾剑波 <gujianbobo@gmail.com>
2023-06-29 10:12:51 +0800

    add

--- recall/att2i/u2i.sh
+++ recall/att2i/u2i.sh
@@ -9,8 +9,8 @@ nohup python -u att2i_train.py --train_file ${root}/cand/u2i_train \
   --test_file ${root}/cand/u2i_test --step_lr_size 1 \
   --save_path ${root}/models/ --log_file ${root}/log/u2i.log \
   --d_model 128 --d_ff 128 --temperature 0.1 --train_batch_size 512 \
-  --epoch 60 --lr 1e-3 --step_lr_size 3 \
-  --weight_decay 0.001 --save_step 300000 > log 2>&1 &
+  --epoch 60 --lr 1e-3 --step_lr_size 5 \
+  --weight_decay 0.01 --save_step 50000 > log 2>&1 &
 
 nohup python -u att2i_item_inference.py \
   --test_file ${root}/data/product2id.dict --log_file ${root}/log/u2i.log --d_model 128 --d_ff 128 \
